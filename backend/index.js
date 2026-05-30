b2eb44a8c49c0361654b4bf3831e2b7d3284e6b5
anuj paul <paulanuj27@gmail.com>
2026-05-30 06:41:08 +0600

    Connect mongoDB with config file and by mongoose package

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const connectDB = require('./config/db.connect')
 require('dotenv').config()
 const app = express()
 
 // Middleware
 app.use(cors())
 app.use(express.json())
+
+// Connect to MongoDB
+connectDB()
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`)
+})
