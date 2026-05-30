76a184173453d988ac8b8d502f992033617e12a9
anuj paul <paulanuj27@gmail.com>
2026-05-30 20:05:27 +0600

    Create email services, send OTP to the email, package name is nodemailer

---
+++ backend/services/email.services.js
@@ -0,0 +1,61 @@
+const nodemailer = require('nodemailer')
+require('dotenv').config()
+
+// Create transporter to the send OTP
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    }
+})
+
+
+// transporter ready or not for send OTP
+transporter.verify((error,success)=> {
+    if (error)
+    {
+        console.error('Gmail services connection failed.');
+    }
+    else 
+    {
+        console.log('Ready for send OTP to email ');
+    }
+})
+
+const sendOtpToEmail = async (email)=>
+{
+      const html = `
+    <div style="font-family: Arial, sans-serif; color: #333; line-height: 1.6;">
+      <h2 style="color: #075e54;">🔐 BDChat Web Verification</h2>
+      
+      <p>Hi there,</p>
+      
+      <p>Your one-time password (OTP) to verify your BDChat Web account is:</p>
+      
+      <h1 style="background: #e0f7fa; color: #000; padding: 10px 20px; display: inline-block; border-radius: 5px; letter-spacing: 2px;">
+        ${otp}
+      </h1>
+
+      <p><strong>This OTP is valid for the next 5 minutes.</strong> Please do not share this code with anyone.</p>
+
+      <p>If you didn’t request this OTP, please ignore this email.</p>
+
+      <p style="margin-top: 20px;">Thanks & Regards,<br/>BDChat Web Security Team</p>
+
+      <hr style="margin: 30px 0;" />
+
+      <small style="color: #777;">This is an automated message. Please do not reply.</small>
+    </div>
+  `;
+
+  await transporter.sendMail({
+    from: `BDChat web ${process.env.EMAIL_USER}`,
+    to: email,
+    subject: 'BDChat verification code.',
+    html,
+  })
+
+}
+
+module.exports = sendOtpToEmail
