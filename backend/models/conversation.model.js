c2b2367e030262cc243c4bdffec36af0587b22d3
anuj paul <paulanuj27@gmail.com>
2026-05-30 09:15:59 +0600

    Create conversation model that can be find user who can chat now and his details like last message, how many unread message you inbox, who can send last message here

---
+++ backend/models/conversation.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const conversationSchema = new mongoose.Schema({
+    // why it is array?.. participant are two user sender and receiver 
+    participants: [{type: mongoose.Schema.Types.ObjectId, ref: 'user'}],
+    lastMessage: {type: mongoose.Schema.Types.ObjectId, ref: 'message'},
+    unreadCount: {
+        type: Number,
+        default: 0,
+    }
+    
+}, {timestamp:true})
+
+
+const conversationModel = mongoose.model('conversation', conversationSchema)
+module.exports = conversationModel 
