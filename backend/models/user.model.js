cb7ba291415c0071a2b0c7df2378eb35dea5fb06
anuj paul <paulanuj27@gmail.com>
2026-05-30 08:55:28 +0600

    Create userModel for create user profile in this application

---
+++ backend/models/user.model.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    phoneNumber: {
+        type: String,
+        unique: true,
+        parse: true,
+    },
+    phoneSuffix: {
+        type: String,
+        unique: false,
+    },
+    username: {type: String},
+    email: {
+        type: String,
+        lowercase: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: 'Invalid Email Addresss.'
+        }
+    },
+    emailOpt: {type:String},
+    emailOptExpiry: {type: Date},
+    profilePicture: {type: String},
+    about: {type: String},
+    lastSeen: {type: Date},
+    isonline: {type: Boolean, default: false},
+    isVerified: {type: Boolean, default: false},
+    agreed: {type: Boolean, default: false},
+
+}, {timestamps: true})
+
+
+const userModel = mongoose.model('user', userSchema)
+module.exports = userModel
