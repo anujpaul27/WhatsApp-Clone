ea7dbdf3245c1fdc0b1d98e6e72b6a180d07f79a
anuj paul <paulanuj27@gmail.com>
2026-05-31 12:59:14 +0600

    Create auth middleware

---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const response = require('../utils/response.handler');
+
+
+const authMiddleware = (req,res,next) =>
+{
+    const token = req.cookies.auth_token;
+
+    if(!token)
+    {
+        return response(res,401,'Token not found.')
+    }
+
+    try 
+    {
+        const decode = jwt.verify(token,process.env.JWT_SECRET) 
+        req.user = decode;
+        console.log(req.user)
+        next()
+    }
+    catch (err)
+    {
+        console.error(err.message)
+        response(res,401,'invalid or expired token.')
+    }
+}
+
+module.exports = authMiddleware
